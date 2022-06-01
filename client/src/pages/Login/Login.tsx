990c24d901ff829db6d389c93747474a71a224f2
farisnevaljalovic <faris.nevaljalovic@live.com>
2022-06-02 01:18:32 +0200

    Login

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,37 +1,48 @@
-import React from 'react';
-import Logo from '../../components/Logo/Logo';
+import React from "react";
+import Logo from "../../components/Logo/Logo";
 import style from "./Login.module.css";
-// props: { getName: Function }
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 function Login(props: { getName: Function }) {
-    const createNewName = () => {
-        let first = '';
-        let char = '';
-        let num = '';
-        let firstLetter = 'AEIOUaeiou';
-        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        let numbers = '1234567890';
+  const user = localStorage.getItem("user");
 
-        first += firstLetter.charAt(Math.floor(Math.random() * firstLetter.length));
+  useEffect(() => {
+    if (user) return props.getName(user);
+  }, []);
 
-        for (let i = 0; i < 4; i++) {
-            char += characters.charAt(Math.floor(Math.random() * characters.length))
-        }
-        for (let i = 0; i < 2; i++) {
-            num += numbers.charAt(Math.floor(Math.random() * numbers.length))
-        }
-        return props.getName(`${first}${char}-${num}`);
-    };
+  const createNewName = () => {
+    let first = "";
+    let char = "";
+    let num = "";
+    let firstLetter = "AEIOUaeiou";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let numbers = "1234567890";
 
-    return (
-        <div className="container">
-            <div className={style.box}>
-                <Logo />
-                <h2>Welcome</h2>
-                <p>Please click the button and start conversation</p>
-                <button className={style.btn} onClick={createNewName}>Get name</button>
-            </div>
-        </div>
-    )
+    first += firstLetter.charAt(Math.floor(Math.random() * firstLetter.length));
+
+    for (let i = 0; i < 4; i++) {
+      char += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    for (let i = 0; i < 2; i++) {
+      num += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+
+    localStorage.setItem("user", `${first}${char}-${num}`);
+    return props.getName(`${first}${char}-${num}`);
+  };
+
+  return (
+    <div className="container">
+      <div className={style.box}>
+        <Logo />
+        <h2>Welcome</h2>
+        <p>Please click the button and start conversation</p>
+        <button className={style.btn} onClick={createNewName}>
+          Get name
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
